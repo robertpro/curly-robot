eba3d1c4db2b19357107f7aa5541bcf9bb891995
José Roberto Meza Cabrera <robertpro01@gmail.com>
2018-10-20 23:20:01 -0500

    Finished detect_color test

--- opencv/detect_color.py
+++ opencv/detect_color.py
@@ -9,47 +9,48 @@ image = cv2.imread("../images/figuras/green.png")
 image = cv2.imread("../images/figuras/yellow.png")
 image = cv2.imread("../images/figuras/base2.png")
 
+
 # define the list of boundaries
-boundaries = [
-    # ([15, 15, 110], [90, 90, 255]),  # Red
-    ([86, 31, 4], [220, 96, 50]),  # Blue
-    # ([25, 116, 160], [110, 194, 250]),  # Yellow
-    # ([45, 70, 10], [100, 250, 90]),  # Green
-]
+
+
+def boundary(x: list) -> np.array:
+    return np.array(x, dtype="uint8")
+
+
+boundaries = {
+    'red': {
+        'lower': boundary([15, 15, 110]),
+        'upper': boundary([90, 90, 255]),
+    },
+    'blue': {
+        'lower': boundary([86, 31, 4]),
+        'upper': boundary([220, 96, 50]),
+    },
+    'yellow': {
+        'lower': boundary([25, 116, 160]),
+        'upper': boundary([110, 194, 250]),
+    },
+    'green': {
+        'lower': boundary([45, 70, 10]),
+        'upper': boundary([100, 250, 90]),
+    },
+}
 
 masks = []
 
 # loop over the boundaries
-for (lower, upper) in boundaries:
-    # create NumPy arrays from the boundaries
-    lower = np.array(lower, dtype="uint8")
-    upper = np.array(upper, dtype="uint8")
+for color in boundaries.keys():
+    lower = boundaries[color].get('lower')
+    upper = boundaries[color].get('upper')
 
-    # find the colors within the specified boundaries and apply
-    # the mask
     mask = cv2.inRange(image, lower, upper)
-    # output = cv2.bitwise_and(image, image, mask=mask)
-
-    # show the images
-    # cv2.imshow("images", np.hstack([image, output]))
-    # cv2.waitKey(0)
 
     masks.append(mask)
 
-if len(masks) > 2:
-    new_mask = None
-    for i in range(len(masks) - 1):
-        lower = np.array(boundaries[0][0], dtype="uint8")
-        upper = np.array(boundaries[0][1], dtype="uint8")
-        if new_mask is None:
-            new_mask = cv2.inRange(image, lower, upper)
-            continue
-        new_mask = cv2.bitwise_or(new_mask, masks[i + 1])
 
-elif len(masks) == 2:
-    new_mask = cv2.bitwise_or(*masks)
-else:
-    new_mask = cv2.inRange(image, lower, upper)
+new_mask = masks[0]
+for i in range(len(masks) - 1):
+    new_mask = cv2.bitwise_or(new_mask, masks[i + 1])
 
 output = cv2.bitwise_and(image, image, mask=new_mask)
 
