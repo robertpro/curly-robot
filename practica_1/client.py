131dcdb66b26bdd96ba38a7b7421cccfdaadd356
reycobra <reycobra.sgo@gmail.com>
2018-11-30 22:46:55 -0600

    Adding argument support to client.py

--- practica_1/client.py
+++ practica_1/client.py
@@ -1,13 +1,45 @@
 #!/usr/bin/env python3
 
+# socket_echo_client.py
 import socket
+import sys
+import click
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
+def connect():
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    # Connect the socket to the port where the server is listening
+    server_address = ('localhost', 10000)
+    print('connecting to {} port {}'.format(*server_address))
+    sock.connect(server_address)
+    return sock
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
 
-print('Received', repr(data))
+@click.command()
+@click.option('--message', help="Message to send")
+def send_message(message):
+    sock = connect()
+    try:
+    
+        # Send data
+        #message = b'This is the message.  It will be repeated.'
+        #print('sending {!r}'.format(message))
+        print('sending ' + message)
+        sock.sendall(message.encode())
+    
+        # Look for the response
+        amount_received = 0
+        amount_expected = len(message)
+    
+        while amount_received < amount_expected:
+            data = sock.recv(16)
+            amount_received += len(data)
+            print('received {!r}'.format(data))
+    
+    finally:
+        print('closing socket')
+        sock.close()
+
+if __name__ == "__main__":
+    send_message()
