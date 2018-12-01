0053c26a3951b03cb6b47b04d142c71bd7201693
reycobra <reycobra.sgo@gmail.com>
2018-12-01 02:04:00 -0600

    Adding photos trigger

---
+++ trigger.sh
@@ -0,0 +1,9 @@
+#!/bin/env bash
+DIR="/home/reycobra/legoman/curly-robot/photos"
+inotifywait -m -r -e create "$DIR" | while read f
+
+do
+    # you may want to release the monkey after the test :)
+    echo monkey
+    # <whatever_command_or_script_you_liketorun>
+done
