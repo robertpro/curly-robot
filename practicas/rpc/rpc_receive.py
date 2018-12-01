ad492a519600a6045150085dbe870bdeffd02c93
Oscar de la Torre <oskr@mx1.ibm.com>
2018-12-01 09:31:00 -0600

    adding RPC

--- practicas/rpc/rpc_receive.py
+++ practicas/rpc/rpc_receive.py
@@ -3,12 +3,6 @@
 # Echo server program
 import socket
 
-dictionary = {
-    "1": "Hello World",
-    "2": "Sistemas Distribuidos",
-    "3": "Practica de RMI"
-}
-
 HOST = ''                 # Symbolic name meaning the local host
 PORT = 50007              # Arbitrary non-privileged port
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -20,9 +14,6 @@ while 1:
     data = conn.recv(64)
     if not data: 
         break
-    if data in dictionary:
-        print (dictionary[data])
-    else:
-        print ("Error")
+    print (dictionary[data])
 conn.close() 
 
