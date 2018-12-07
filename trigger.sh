4aa74be61883cfc0e00e619937eb500886fc9864
reycobra <reycobra.sgo@gmail.com>
2018-12-07 07:37:20 -0600

    Adding more trigger funcionality

--- trigger.sh
+++ trigger.sh
@@ -4,6 +4,6 @@ inotifywait -m -r -e create "$DIR" | while read f
 
 do
     # you may want to release the monkey after the test :)
-    echo monkey
+    /home/$USER/legoman/curly-robot/measurement_test/black_board.py --image $(ls -tr images/ | tail -n 1)
     # <whatever_command_or_script_you_liketorun>
 done
