3287691badec12deb072a2bc0610d6f51a625a3a
José Roberto Meza Cabrera <robertpro01@gmail.com>
2018-12-07 19:51:13 -0600

    removing hardcoded PATH

--- trigger.sh
+++ trigger.sh
@@ -1,9 +1,9 @@
 #!/bin/env bash
-DIR="/home/reycobra/legoman/curly-robot/images"
-inotifywait -m -r -e create "$DIR" | while read f
+DIR="./images"
 
+inotifywait -m -e create "$DIR" | while read f
 do
     # you may want to release the monkey after the test :)
-    /home/$USER/legoman/curly-robot/measurement_test/black_board.py --image $(ls -tr images/ | tail -n 1)
+    ./measurement_test/black_board.py --image $(ls -tr ${DIR}/ | tail -n 1)
     # <whatever_command_or_script_you_liketorun>
 done
