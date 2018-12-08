babdbeffaa24f9655edf4e44a3960a867508524d
reycobra <reycobra.sgo@gmail.com>
2018-12-08 07:43:57 -0600

    Adding width coefficient, get_img function and take_pic function

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+#!/usr/bin/env python
+
 import argparse
-
 from lego.EV3_Send import send_to_lego
-from measurement_test.black_board import find_color, find_board
-
+from measurement_test.black_board import find_obj, find_board, take_pic, get_img
 from measurement_test import black_board
 
 HOST = '192.168.43.191'  # The remote host
@@ -12,8 +12,7 @@ PORT = 50009  # The same port as used by the server
 def main():
     # construct the argument parse and parse the arguments
     ap = argparse.ArgumentParser()
-    ap.add_argument("-i", "--image", help="path to the image file",
-                    required=True)
+    ap.add_argument("-i", "--image", help="path to the image file")
     ap.add_argument("-H", "--host", help="host to connect", default=HOST)
     ap.add_argument("-p", "--port", help="port to connect", default=PORT)
 
@@ -21,12 +20,15 @@ def main():
     host = args.get('host')
     port = args.get('port')
     image_name = args.get('image')
+    if image_name:
+        image = get_img(image_name)
+    else:
+        image = take_pic()
 
-    cm_pixel_heigth_coefficient, cm_pixel_width_coefficient = find_board(image_name=image_name)
-
+    heigth_coefficient, width_coefficient = find_board(image=image)
     for color in black_board.COLORS.keys():
         print(f"Finding color: {color}")
-        x, y = find_color(image_name, color, cm_pixel_heigth_coefficient, cm_pixel_width_coefficient)
+        x, y = find_obj(image, color, heigth_coefficient, width_coefficient)
         print(f"Color found: {color} | X:{x}, Y:{y}")
 
         print(f"Sending lego to {x}, {y} for color {color}")
