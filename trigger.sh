9b6a7f41ddfa503fd4bbb60024c6f43f12e263ee
reycobra <reycobra.sgo@gmail.com>
2018-12-08 08:01:10 -0600

    Removing trigger.sh

--- trigger.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/env bash
-DIR="./images"
-
-inotifywait -m -e create "$DIR" | while read f
-do
-    # you may want to release the monkey after the test :)
-    ./measurement_test/black_board.py --image $(ls -tr ${DIR}/ | tail -n 1)
-    # <whatever_command_or_script_you_liketorun>
-done
